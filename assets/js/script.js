320950bfce2e9b6835b2a161d9a70bc3b47c15f6
MuriloPolicastro <murilopolicastro@hotmail.com>
2023-10-19 17:26:49 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,34 @@ Pegar nome do cliente e enviar para o campo cliente
 pegar data e enviar para campo data
 pegar end e enviar para campo end
 */
+function EnvDados () {
+    const iptCliente = document.querySelector('#cliente');
+    const rcbCliente = document.querySelector('.nome-cliente');
+    const iptData = document.querySelector('#input-data');
+    const rcbData = document.querySelector('.data-recibo');
+    const iptEnd = document.querySelector('#input-end');
+    const rcbEnd = document.querySelector('.end-recibo');
 
-const iptCliente = document.querySelector('#cliente');
-const rcbCliente = document.querySelector('.nome-cliente');
+    this.pressBtn = function() {
+        document.addEventListener ('click', e => {
+            const el = e.target;
 
-document.addEventListener ('click', function(e) {
-    const el = e.target;
-
-    if (el.classList.contains('env-tudo')) {
-        rcbCliente.innerHTML = `Cliente: ${iptCliente.value}`;
+            if (el.classList.contains('env-tudo')) {
+                this.envInfo(iptCliente.value, iptData.value, iptEnd.value);
+            };
+        });
     };
-});
+
+    this.envInfo = function(nome, data, end) {
+        rcbCliente.innerHTML = `Cliente: ${nome}`;
+        rcbData.innerHTML = `Data: ${data}`;
+        rcbEnd.innerHTML = `Endereço para Instalação: ${end}`;
+    };
+
+    this.formtData = function () {
+        
+    };
+}
+
+const enviar = new EnvDados();
+enviar.pressBtn();
